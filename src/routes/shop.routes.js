936d682467e4d25ef6629dbdceb78b21d69c6b8f
amperefz <amperefz@gmail.com>
2023-12-09 18:39:49 -0300

    Algunas correcciones

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get('/shop', (req, res) => res.send('Esta es la vista de Tienda'));
 router.get('/shop/item/:id', (req, res) => res.send('Esta es la vista de Ítem'));
-router.post('/shop/item/:id/add', (req, res) => res.send('Esta es la vista de Crear ítem'));
+router.post('/shop/item/:id/add', (req, res) => res.send('Esta es la vista de Agregar ítem al carrito'));
 router.get('/shop/cart', (req, res) => res.send('Esta es la vista del Carrito de Compras'));
-router.post('/shop/cart', (req, res) => res.send('Esta es la vista del Carrito de Compras'));
+router.post('/shop/cart', (req, res) => res.send('Esta es la vista del Checkout del Carrito de Compras'));
 
 module.exports = router;
