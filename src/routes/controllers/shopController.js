3c8fdf2621ab2a49087bc79efffdeb2ad0493b4f
amperefz <amperefz@gmail.com>
2023-12-09 20:13:30 -0300

    Cambios

--- src/routes/controllers/shopController.js
+++ src/routes/controllers/shopController.js
@@ -1,7 +1,7 @@
 const shopControllers = {
     shop:(req, res) => res.send('Ruta para Tienda'),
-    item: (req, res) => (req, res) => res.send('Ruta para Ítem'),
-    cart: (req, res) => (req, res) => res.send('Ruta para Carrito de Compras'),
+    item: (req, res) => res.send('Ruta para Ítem'),
+    cart: (req, res) => res.send('Ruta para Carrito de Compras'),
 }
 
 module.exports = shopControllers;
